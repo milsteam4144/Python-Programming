80f70967e222212c4b66534b9f4f54b5f5fbf4a1
Mallory Milstead <mallorymilstead@gmail.com>
2026-01-15 19:16:13 +0000

    very basic data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,1 +1,23 @@
 # Mallory Milstead
+
+import pandas as pd
+
+# Read the csv file into a variable
+df = pd.read_csv("home_school_state.csv")
+
+# Display first few rows in the dataframe
+print(df.head(10))
+
+# Put the homeschool_students column into a variable
+student_count = df["homeschool_students"]
+
+# Display largest student count
+#print(student_count.max())
+
+largest_stud_count = df[df['homeschool_students'] == df['homeschool_students'].max()]
+print("Largest number of homeschool students")
+print()
+print(largest_stud_count)
+
+# Display smallest student count
+#print(student_count.min())
